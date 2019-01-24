185afb0a20830557f81fcdfccc67a8febfe0a5c0
clifordsymack <clifordsymack@some.where>
2019-01-24 22:45:56 +0300

    add ws testing

---
+++ ws_example.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const magic = "42bcc32669467873";
+
+const client = new WebSocket('wss://cashshuffle.c3-soft.com');
+
+client.on('open', function open() {
+  hi_msg = new Buffer(magic + "00000014" + "0a120a101a090a07736f6d656b65793a0308e807", "hex");
+  cleint.send(hi_msg);
+})
+
+client.on('message', function incoming(message){
+  console.log(message);
+})
+
+client.on('error', function(error){
+    console.log('errrorrrrs here')
+    console.log(error);
+})
